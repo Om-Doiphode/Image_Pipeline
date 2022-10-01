7a5153786e8e8e92d00286ea23c97af78f1c2dbd
KedarDhamankar <97346542+KedarDhamankar@users.noreply.github.com>
2022-10-01 13:26:55 +0530

    Gamma Correction Algorithm

--- GammaCorrection.cpp
+++ GammaCorrection.cpp
@@ -1,26 +1,48 @@
+/*
+* Author List: Kedar Dhamankar
+* Filename: GammaCorrection.cpp
+* Functions: GammaCorrection
+* Global Variables: height, width
+* variables>
+*/ 
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include<math.h>
 using namespace std;
 
+const int height = 1324;
+const int width = 2012;
+
+
+/*
+* Function Name: GammaCorrection
+* Input: filename -> image which is to be gamma corrected and stores corrected values in another file
+* Output: Performs Gamma Correction and creates a file having corrected values
+          and Returns no value to the function
+* Logic: First loops through the file and stores each value in X1, then applies gamma correction
+         and stores corrected values in X2 and then creates file2 and stores corrected values in it
+* Example Call: GammaCorrection("R.txt", "RedGamma.txt");
+*/
+
+
 void GammaCorrection (string file1, string file2)
 {
-    ifstream f1(file1);
-    ofstream f2(file2, ios::out);
-    double X1, X2;
+    ifstream f1(file1);                         //Opens file1 in read mode
+    ofstream f2(file2, ios::out);               //Opens file2 in write mode
+    double X1;                                  //for storing each value of file1
+    double X2;                                  //for storing corrected values
 
-    for( int i=0; i<480; i++) 
+
+    for( int i=0; i<height; i++)                //Looping through the file
     {
-        for (int j=0; j<640; j++) 
+        for (int j=0; j<width; j++) 
         {
-            f1>>X1;
-            // double Y1 = X1/256;
-            X2 = pow(X1,0.45);
-            // cout<<"X1 = "<<X1<<endl;
-            // cout<<"X2 = "<<X2<<endl;
-            // X2 = 255*Y2;
-            f2<<X2<<" ";
+            f1>>X1;                             //Storing value in X1
+            X2 = pow(X1,0.45);                  //Applying gamma correction to it
+            f2<<X2<<" ";                        //Writing the value in file2 
         }
         f2<<endl;
     }
@@ -28,7 +50,8 @@ void GammaCorrection (string file1, string file2)
 
 int main()
 {
-    GammaCorrection("RedScaled.txt", "RedG.txt");
-    GammaCorrection("GreenScaled.txt", "GreenG.txt");
-    GammaCorrection("BlueScaled.txt", "BlueG.txt");
+    //Calling the function GammaCorrection which takes in one image channel and creates and writes in a new image channel having corrected values
+    GammaCorrection("R.txt", "RedGamma.txt");
+    GammaCorrection("G.txt", "GreenGamma.txt");
+    GammaCorrection("B.txt", "BlueGamma.txt");
 }
