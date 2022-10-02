39d0ef3d0ec882fe412c764faafedb314cfcc7af
KedarDhamankar <97346542+KedarDhamankar@users.noreply.github.com>
2022-10-02 16:13:12 +0530

    Update auto_exposure.cpp

--- src/auto_exposure.cpp
+++ src/auto_exposure.cpp
@@ -12,18 +12,17 @@ using matrix=vector<vector<double>>;
 
 /*
 * Function Name: Find_Mean
-* Input: Passes filename parameter which finds mean value of the file 
-* Output: Returns the mean value of the file having datatype -> double
-* Logic: Loops through the file and adds each value to 'sum' variable at each iteration
-         Then divides the sum by total number of values in the file to find Mean 
-* Example Call: Find_Mean("RedScaled.txt");   
-                where RedScaled.txt is a file having normalised values of image's Red Channel
+* Input: Passes 2d image vector of whose mean value is to be calculated 
+* Output: Returns the mean value of the image matrix
+* Logic: Loops through the matri and adds each value to 'sum' variable at each iteration
+         Then divides the sum by total number of values in the vector to find Mean 
+* Example Call: Find_Mean(image);
 */
 
 double Find_Mean(matrix image)
-{                  //Opens the file in read mode
-    double sum = 0;                         //declare sum variable to store sum of all value of file
-    for ( int i=0; i<image.size(); i++)           //Looping through the whole file
+{                 
+    double sum = 0;                               //declare sum variable to store sum of all value of matrix
+    for ( int i=0; i<image.size(); i++)           //Looping through the matrix
     {
         for (int j=0; j<image[0].size(); j++)
         {
@@ -32,30 +31,28 @@ double Find_Mean(matrix image)
     }
 
 
-    double mean = sum/(image.size()*image[0].size());       //Calculates the mean value of file
+    double mean = sum/(image.size()*image[0].size());       //Calculates the mean value of matrix
     cout<<"Sum = "<<sum<<" "<<endl;
     cout<<"Mean = "<<mean<<" "<<endl;
 
-    return mean;                            //Returns the mean value of the file to the function
+    return mean;                            //Returns the mean value of the matrix to the function
 }
 
 
 /*
 * Function Name: AutoExposure
-* Input: Passes 2 filename parameters which will read first file and stored Auto Exposure corrected values in another file
-         and passes mean value of the file which was return by the Find_Mean function
+* Input: Passes image matrix and mean value of that matrix
 * Output: Returns no value to the function
 * Logic: Image channel having normalised values in the range 0-1 is run through a loop
          where each pixel value is compared to the mean intensity value of the image
          and correction is applied accordingly
-* Example Call: AutoExposure("GreenScaled.txt","GreenAE.txt", Green_mean);
+* Example Call: AutoExposure(image, Green_mean);
 */ 
 
 matrix AutoExposure(matrix image, double mean)
 {
     matrix res(image.size(),vector<double>(image[0].size(),0));
-    double X1;                                  //X1 stores the value of file1 in each iteration compares it to mean value
-    double X2;                                  //X2 stores the corrected intensity value of each pixel
+    double X1;                                  //X1 stores each value of image matrix in each iteration and compares it to mean value
 
 
     for (int i=0; i<image.size(); i++)                //Looping through the image channels
